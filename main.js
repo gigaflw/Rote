8ace443828894fc8c716934e65fa96c5f03c5998
zhouben <a_gigantic_flower@yeah.net>
2017-09-03 15:20:45 +0800

    App barebone

---
+++ main.js
@@ -0,0 +1,52 @@
+/*
+* @Author: gigaflower
+* @Date:   2017-09-01 10:47:10
+* @Last Modified by:   gigaflower
+* @Last Modified time: 2017-09-01 10:56:38
+*/
+
+const http = require('http');
+const fs = require('fs');
+const console = require('console');
+const csvParse = require('csv-parse/lib/sync');
+
+const lexRoot = __dirname + '/lexicons';
+const webRoot = __dirname + '/src';
+
+const server = http.createServer((req, resp) => {
+    // TODO: os-adaptive path
+    // TODO: UTF-8 filename
+    // TODO: better server
+    if (req.url === '/lex/all') {
+        let files = fs.readdirSync(lexRoot);
+        let lexNames = files
+            .filter(f => f.endsWith('.csv'))
+            .map(f => ({name: f.slice(0, -4)}));
+        resp.end(JSON.stringify(lexNames));
+    } else if (req.url.match(/^\/lex\/.+$/)) {
+        let filename = req.url.match(/^\/lex\/(.+)$/)[1];
+        if (filename === 'default') filename = 'jp-alphabet';
+
+        fs.readFile(`${lexRoot}/${filename}.csv`, (err, data) => {
+            if (err) {
+                resp.writeHeader(404);
+                resp.end(`"${filename}.csv" can not be found`);
+            } else {
+                let [header, ...entries] = csvParse(data);
+                resp.end(JSON.stringify({
+                    header, entries, name: filename,
+                }));
+            }
+        });
+    } else {
+        let filename = webRoot + (req.url === '/' ? '/App.html' : req.url);
+        fs.readFile(filename, (err, data) => {
+            if (err) {
+                resp.writeHeader(404);
+                resp.end("No file found");
+            } else {
+                resp.end(data);
+            }
+        });
+    }
+}).listen(12345);
---
+++ src/App.js
@@ -0,0 +1,157 @@
+{
+    function ajax(url) {
+        return new Promise((resolve, reject) => {
+            let client = new XMLHttpRequest();
+            client.open('GET', url);
+            client.onreadystatechange = function () {
+                if (this.readyState === XMLHttpRequest.DONE) {
+                    if (this.status === 200) {
+                        resolve(this.response);
+                    } else {
+                        reject(new Error(this.responseText));
+                    }
+                }
+            };
+            client.send();
+        });
+    }
+
+    let app = {
+        init(lexicon = undefined) {
+            if (lexicon === undefined) {
+                this.setLexicon('default');
+            } else {
+                console.log(lexicon);
+                let {name, header, entries} = lexicon;
+
+                this.getCurDictName = () => name;
+                this.getCurDictHeaders = () => header;
+
+                let wordInd = 0;
+
+                this.curWord = () => entries[wordInd];
+                this.nextWord = () => {
+                    let nextInd = 0;
+                    if (entries.length <= 1) {
+                        alert('词库耗尽')
+                    } else {
+                        do {
+                            nextInd = Math.floor(Math.random() * (entries.length))
+                        } while (nextInd === wordInd);
+                        wordInd = nextInd;
+                    }
+                    return this.curWord();
+                };
+            }
+        },
+
+        getAllDicts() {
+            return ajax('/lex/all')
+        },
+        setLexicon(name) {
+            if (this.getCurDictName && name === this.getCurDictName()) {
+                console.log(`Already in lexicon '${name}'`);
+            }
+            console.log(name);
+            return ajax('/lex/' + name).then(data => {
+                data = JSON.parse(data);
+                this.init(data);
+                UI.init();
+            })
+        },
+    };
+
+    let UI = {
+        init() {
+            let screen = document.getElementById('screen');
+            let panel = document.getElementById('panel');
+            screen.innerHTML = panel.innerHTML = '';
+
+            function initScreen(barCnt) {
+                let str = '';
+                for (let i = 0; i < barCnt; ++i) {
+                    str += `<p><span class="word-slot hidden"></span></p>`;  // hidden by default
+                }
+                screen.innerHTML = str;
+            }
+
+            function initPanelCommands(commands) {
+                let list = document.createElement('ul');
+
+                for (let cmd of commands) {
+                    let el = document.createElement('li');
+                    el.innerHTML = cmd.prompt;
+                    el.addEventListener('click', cmd.handler);
+                    list.appendChild(el);
+                }
+                panel.appendChild(list);
+            }
+
+            function initPanelLexicons(lexicons) {
+                let list = document.createElement('ul');
+
+                for (let lex of lexicons) {
+                    let el = document.createElement('li');
+                    el.innerHTML = lex.name;
+                    el.classList.add('lex-name');
+                    el.addEventListener('click', event => app.setLexicon(lex.name));
+                    list.appendChild(el);
+                }
+                panel.appendChild(list);
+            }
+
+            initScreen(app.getCurDictHeaders().length);
+            initPanelCommands([
+                ...app.getCurDictHeaders().map(header => ({
+                    prompt: header,
+                    handler: event => {
+                        event.target.classList.toggle('active');
+                        this.toggleWordVisibility(
+                            Array.from(event.target.parentNode.childNodes).indexOf(event.target)
+                        )
+                    }
+                })), {
+                    prompt: 'next',
+                    handler: event => this.setWord(app.nextWord()),
+                }
+            ]);
+
+            this.wordSlots = document.getElementsByClassName('word-slot');
+
+            // make the first word slot visible
+            panel.querySelector('li').classList.add('active');
+            this.toggleWordVisibility(0);
+            this.setWord(app.nextWord());
+
+            // get a list of all available lexicons
+            app.getAllDicts().then(data => {
+                data = JSON.parse(data);
+                initPanelLexicons(data);
+                let curDict = app.getCurDictName();
+                for (let el of document.getElementsByClassName('lex-name')) {
+                    if (el.innerHTML === curDict) {
+                        el.classList.add('active');
+                    } else {
+                        el.classList.remove('active');
+                    }
+                }
+            });
+        },
+
+        setWord(word) {
+            for (let ind = 0; ind < this.wordSlots.length; ++ind) {
+                this.wordSlots[ind].innerHTML = word[ind];
+            }
+        },
+
+        toggleWordVisibility(ind) {
+            if (ind >= this.wordSlots.length) {
+                throw RangeError(`${ind} out of range of word slots`)
+            } else {
+                this.wordSlots[ind].classList.toggle('hidden');
+            }
+        },
+    };
+
+    app.init();
+}
