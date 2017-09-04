ae16ec6069e84d83d6ca59e94891b357510bb198
zhouben <a_gigantic_flower@yeah.net>
2017-09-04 11:17:16 +0800

    traverse order added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 {
+    Array.prototype.shuffle = function () {
+        for (let i = 0; i < this.length; ++i) {
+            let j = Math.floor(Math.random() * this.length);
+            if (i !== j) [this[i], this[j]] = [this[j], this[i]];
+        }
+    };
+
     function ajax(url) {
         return new Promise((resolve, reject) => {
             let client = new XMLHttpRequest();
@@ -29,15 +36,35 @@
                 let wordInd = 0;
 
                 this.curWord = () => entries[wordInd];
+
+                let traverseMethod = 'random';  // available methods: 'default', 'random', 'shuffle'
+                let shuffleTable =  entries.map((val, ind) => ind);
+                let shuffledInd = 0;
+                this.getAllTraverseMethods = () => ['random', 'shuffle', 'default'];
+                this.getTraverseMethod = () => traverseMethod;
+                this.setTraverseMethod = method => {
+                    traverseMethod = method;
+                    if (method === 'shuffle') shuffleTable.shuffle();
+                    console.log(`traverse method set to '${method}'`);
+                };
+
                 this.nextWord = () => {
-                    let nextInd = 0;
                     if (entries.length <= 1) {
                         alert('词库耗尽')
                     } else {
-                        do {
-                            nextInd = Math.floor(Math.random() * (entries.length))
-                        } while (nextInd === wordInd);
-                        wordInd = nextInd;
+                        if (traverseMethod === 'default') {
+                            wordInd = (wordInd + 1) % entries.length;
+                        } else if (traverseMethod === 'random') {
+                            let nextInd;
+                            do {
+                                nextInd = Math.floor(Math.random() * (entries.length))
+                            } while (nextInd === wordInd);
+                            wordInd = nextInd;
+                        } else if (traverseMethod === 'shuffle') {
+                            shuffledInd = (shuffledInd + 1) % entries.length;
+                            if (shuffledInd === 0) shuffleTable.shuffle();
+                            wordInd = shuffleTable[shuffledInd];
+                        }
                     }
                     return this.curWord();
                 };
@@ -64,34 +91,6 @@
             let panel = document.getElementById('panel');
             panel.innerHTML = '';
 
-            function initPanelCommands(commands) {
-                let list = document.createElement('ul');
-
-                for (let cmd of commands) {
-                    let el = document.createElement('li');
-                    el.innerHTML = cmd.prompt;
-                    if (cmd.classList) {
-                        cmd.classList.forEach(cls => el.classList.add(cls))
-                    }
-                    el.addEventListener('click', cmd.handler);
-                    list.appendChild(el);
-                }
-                panel.appendChild(list);
-            }
-
-            function initPanelLexicons(lexicons) {
-                let list = document.createElement('ul');
-
-                for (let lex of lexicons) {
-                    let el = document.createElement('li');
-                    el.innerHTML = lex.name;
-                    el.classList.add('lex-name');
-                    el.addEventListener('click', event => app.setLexicon(lex.name));
-                    list.appendChild(el);
-                }
-                panel.appendChild(list);
-            }
-
 
             let headerCnt = app.getCurDictHeaders().length;
             let headIndIncre = x => (x + 1) % headerCnt;
@@ -111,13 +110,23 @@
                 setWord(app.curWord());
             }
 
-            function setWord(word) {
-                shownWordSlot.innerHTML = word[shownHeaderInd];
-                hiddenWordSlot.innerHTML = word[hiddenHeaderInd];
+            function addPanelCommands(commands) {
+                let list = document.createElement('ul');
+
+                for (let cmd of commands) {
+                    let el = document.createElement('li');
+                    el.innerHTML = cmd.prompt;
+                    if (cmd.classList) {
+                        cmd.classList.forEach(cls => el.classList.add(cls));
+                    }
+                    el.addEventListener('click', cmd.handler);
+                    list.appendChild(el);
+                }
+                panel.appendChild(list);
             }
 
-            initPanelCommands([
-                {
+            addPanelCommands([
+                {   // command for visible word slot setting
                     prompt: app.getCurDictHeaders()[shownHeaderInd],
                     handler: event => {
                         if (headerCnt === 0) {
@@ -127,7 +136,7 @@
                             event.target.innerHTML = app.getCurDictHeaders()[shownHeaderInd];
                         }
                     }
-                }, {
+                }, { // command for hidden word slot setting
                     prompt: app.getCurDictHeaders()[hiddenHeaderInd],
                     handler: event => {
                         if (headerCnt === 0) {
@@ -137,20 +146,40 @@
                             event.target.innerHTML = app.getCurDictHeaders()[hiddenHeaderInd];
                         }
                     }
-                }, {
+                }, {  // command for next word
                     prompt: 'next',
                     handler: event => setWord(app.nextWord()),
+                }, {  // command for traverse order
+                    prompt: app.getTraverseMethod(),
+                    handler: event => {
+                        let allMethods = app.getAllTraverseMethods();
+                        let newMethod =
+                            allMethods[(allMethods.indexOf(app.getTraverseMethod()) + 1) % allMethods.length];
+                        event.target.innerHTML = newMethod;
+                        app.setTraverseMethod(newMethod);
+                    },
                 }
             ]);
 
-            // make the first word slot visible
-            // panel.querySelector('li').classList.add('active');
+            function setWord(word) {
+                shownWordSlot.innerHTML = word[shownHeaderInd];
+                hiddenWordSlot.innerHTML = word[hiddenHeaderInd];
+            }
+
             setWord(app.nextWord());
 
-            // get a list of all available lexicons
+            // UI for lexicons
             app.getAllDicts().then(data => {
-                data = JSON.parse(data);
-                initPanelLexicons(data);
+                // get a list of all available lexicons
+                let lexicons = JSON.parse(data);
+
+                // initPanelLexicons(data);
+                addPanelCommands(lexicons.map(lex => ({
+                    prompt: lex.name,
+                    classList: ['lex-name'],
+                    handler: event => app.setLexicon(lex.name),
+                })));
+
                 let curDict = app.getCurDictName();
                 for (let el of document.getElementsByClassName('lex-name')) {
                     if (el.innerHTML === curDict) {
